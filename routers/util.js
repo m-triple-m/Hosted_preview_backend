912d2931c3cfda074e83cfeafdc4845fbb04c98f
m-triple-m <triplem656@gmail.com>
2022-08-01 00:31:23 +0530

    ..

--- routers/util.js
+++ routers/util.js
@@ -1,11 +1,11 @@
 const multer = require('multer');
 const router = require('express').Router();
-const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
-const ffmpeg = require("fluent-ffmpeg");
-ffmpeg.setFfmpegPath(ffmpegPath);
+// const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+// const ffmpeg = require("fluent-ffmpeg");
+// ffmpeg.setFfmpegPath(ffmpegPath);
 
 
-const storage = multer.diskStorage({   
+const storage = multer.diskStorage({
     destination: function(req, file, cb) { 
         cb(null, './static/uploads');    
     }, 
@@ -16,22 +16,22 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage }).single("myfile");
 
-const generateVidPreview = (filename) => {
-  console.log('static/uploads/'+filename);
-  ffmpeg('../static/uploads/'+filename)
-  .setStartTime("00:00:10")
-  .setDuration("5")
-  .output("./static/previews/"+filename+"_preview.gif")
-  .on("end", function (err) {
-    if (!err) {
-      console.log("conversion Done");
-    }
-  })
-  .on("error", function (err) {
-    console.log("error: ", err);
-  })
-  .run();
-}
+// const generateVidPreview = (filename) => {
+//   console.log('static/uploads/'+filename);
+//   ffmpeg('../static/uploads/'+filename)
+//   .setStartTime("00:00:10")
+//   .setDuration("5")
+//   .output("./static/previews/"+filename+"_preview.gif")
+//   .on("end", function (err) {
+//     if (!err) {
+//       console.log("conversion Done");
+//     }
+//   })
+//   .on("error", function (err) {
+//     console.log("error: ", err);
+//   })
+//   .run();
+// }
 
  router.post("/uploadfile", (req, res) => {
     upload(req, res, (err) => {
@@ -44,16 +44,16 @@ const generateVidPreview = (filename) => {
  });
 
 
- router.post("/gen-preview", (req, res) => {
-    upload(req, res, (err) => {
-     if(err) {
-      console.log(err);
-       res.status(400).send("Something went wrong!");
-     }else{
-      generateVidPreview(req.file.originalname);
-      res.json({previewLink : "http://localhost:4000/"+req.file.originalname+"_preview.gif"});
-     }
-   });
- })
+//  router.post("/gen-preview", (req, res) => {
+//     upload(req, res, (err) => {
+//      if(err) {
+//       console.log(err);
+//        res.status(400).send("Something went wrong!");
+//      }else{
+//       generateVidPreview(req.file.originalname);
+//       res.json({previewLink : "http://localhost:4000/"+req.file.originalname+"_preview.gif"});
+//      }
+//    });
+//  })
 
  module.exports = router;
